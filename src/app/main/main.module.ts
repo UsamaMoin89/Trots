8fe3d12891e2df8ba070764c7145c5870223974b
UsamaMoin89 <usamamoin89@gmail.com>
2021-12-13 06:58:45 +0500

    Merging conflicts

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainRouting } from './main.routing';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
     declarations: [
-        DashboardComponent,
         MainComponent
     ],
     imports: [
--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ActiveSessionComponent } from './active-session/active-session.component';
+import { AddQuizesComponent } from './add-quizes/add-quizes.component';
+import { AdminAuthComponent } from './admin-auth/admin-auth.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ManagementRouting } from './management.routing';
-import { TutorsComponent } from './tutors/tutors.component';
-import { StudentsComponent } from './students/students.component';
-import { AccountsComponent } from './accounts/accounts.component';
-import { ActiveSessionComponent } from './active-session/active-session.component';
-import { AddQuizesComponent } from './add-quizes/add-quizes.component';
-import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { AdminAuthComponent } from './admin-auth/admin-auth.component';
+import { StudentsComponent } from './students/students.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { TutorsComponent } from './tutors/tutors.component';
 
 @NgModule({
   declarations: [
